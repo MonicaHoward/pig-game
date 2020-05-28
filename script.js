5cf805a310f84ed5ce02c71200d6775b32a49814
Monica Howard <mo@Monicas-Air.attlocal.net>
2020-05-28 16:48:02 -0500

    update variable naming for clarity

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let activePlayer = 0;
-let diceRoll;
+let diceVal;
 
-let dice = document.querySelector("#roll-dice");
+let diceEl = document.querySelector("#roll-dice");
 let roundScore = document.querySelector("#current-" + activePlayer);
 
 dice.addEventListener("click", function(){
-    console.log("clicked", diceRoll = Math.floor(Math.random() * 6 + 1));
-    roundScore.textContent = diceRoll;
+    console.log("clicked", diceVal = Math.floor(Math.random() * 6 + 1));
+    roundScore.textContent = diceVal;
 
 })
 
