717666df2061a73ebd30de6544665088f803684a
Monica Howard <mo@Monicas-Air.attlocal.net>
2020-05-29 14:04:09 -0500

    toggles between players & uses game logic to collect/delete scores

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 let activePlayer = 0;
 let diceVal;
+let roundScore = 0;
 
 let diceImg = document.querySelector("#dice-img");
 let rollDice = document.querySelector("#roll-dice");
-let roundScore = document.querySelector("#current-" + activePlayer);
 
 diceImg.style.display = 'none';
 
 rollDice.addEventListener("click", function(){
-    console.log("clicked", diceVal = Math.floor(Math.random() * 6 + 1));
-    roundScore.textContent = diceVal;
+    diceVal = Math.floor(Math.random() * 6 + 1);
     diceImg.style.display = 'inline';
     diceImg.src = './assets/images/dice-' + diceVal + ".png";
+
+    if (diceVal > 1){
+        roundScore += diceVal;
+        document.querySelector("#current-" + activePlayer).textContent = roundScore;
+    } else {
+        roundScore = 0;
+        document.querySelector("#current-" + activePlayer).textContent = 0;
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+
+        
+    }
 })
 
 
